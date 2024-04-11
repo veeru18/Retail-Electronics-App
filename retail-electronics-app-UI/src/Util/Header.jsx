5235ed9ad6d61cbe7ae5bb1fd9c7cb64760f8160
Veeresh T A <veereshta@gmail.com>
2024-04-11 15:04:19 +0530

    private and public routes specified

--- retail-electronics-app-UI/src/Util/Header.jsx
+++ retail-electronics-app-UI/src/Util/Header.jsx
@@ -1,8 +1,9 @@
 import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import logo from '../Public/Resources/Electron_Logo.jpg'
-import user from '../Public/Resources/userlogo.png'
+import userlogo from '../Public/Resources/userlogo.png'
 import search from '../Public/Resources/search.png'
+import store from '../Public/Resources/storeicon.png'
 
 function Header() {
   let [isOpen,setIsOpen]=useState(false);
@@ -13,30 +14,38 @@ function Header() {
       <Link className='flex-wrap ml-20 p-2' to={"/"}>
         <img className='rounded-lg' src={logo} style={{height:'50px',width:'100px'}} alt="logo"/>
       </Link>
-      <div className='flex ml-8 mt-2 mb-2 bg-gray-200 rounded'>
-        <img className='absolute ml-2 mt-3 w-6 h-6' src={search} alt="search" />
-        <input autoFocus  style={{width:'40vw'}} className='flex-wrap rounded pl-3 p-1 ml-10 bg-gray-200' placeholder='Search for Products, Brands & more' type="text" name="search" id="searchbox" />
+      <div className='ml-8 mt-2 mb-2 bg-gray-200 rounded'>
+        <button onClick={()=>{}} className='absolute ml-2 mt-3'>
+        <img className='w-6 h-6' src={search} alt="search" />
+        </button>
+        <input autoFocus  style={{width:'30vw'}} className='rounded mt-1 pl-3 p-1 ml-10 bg-gray-200' placeholder='Search for Products, Brands & more' type="text" name="search" id="searchbox" />
       </div>
-      <div className=' hover:bg-blue-500 ml-7 pl-3 pt-2 flex-wrap justify-center rounded'>
-        <Link to={"/login"}>
-          <img className='ml-1 mt-3 w-5 h-5' src={user} alt="" />
-        </Link>
-        <div className='font-medium -mt-8 ml-5 p-2 mr-4' onMouseEnter={()=>{setIsOpen((prev)=>!prev)}} onMouseLeave={()=>{setIsOpen((prev)=>prev)}}>
-        <Link to={"/login"}>Login</Link>
-          {isOpen && ( 
-            <div className=''>
-            {list.map((item,i)=>(
-              <div className=' font-thin absolute -ml-10 pl-7 p-2 mt-5 w-full border-b-4 rounded' onMouseLeave={()=>{setIsOpen((prev)=>!prev)}} onMouseEnter={()=>{setIsOpen((prev)=>true)}}>
-                <Link className='' to={"/register"}>New Customer? </Link> 
-                <Link className='pl-2 font-medium text-blue-700' to={"/register"}>{item}</Link>
+        <div className=' hover:bg-blue-500 ml-7 pl-3 pt-2 flex-wrap justify-center rounded'>
+          <Link to={"/login"}>
+            <img className='ml-1 mt-3 w-5 h-5' src={userlogo} alt="" />
+          </Link>
+          <div className='font-medium -mt-8 ml-5 p-2 mr-4' onMouseEnter={()=>{setIsOpen((prev)=>!prev)}} onMouseLeave={()=>{setIsOpen((prev)=>prev)}}>
+          <Link to={"/login"}>Login</Link>
+            {isOpen && ( 
+              <div className=''>
+              {list.map((item,i)=>(
+                <div className=' font-thin absolute -ml-10 pl-7 p-2 mt-5 w-full rounded' onMouseLeave={()=>{setIsOpen((prev)=>!prev)}} onMouseEnter={()=>{setIsOpen((prev)=>true)}}>
+                  <Link className='' to={"/register"}>New Customer? </Link> 
+                  <Link className='pl-2 font-medium text-blue-700' to={"/register"}>{item}</Link>
+                </div>
+              ))}
               </div>
-            ))}
-            </div>
-          )
-        }
+            )
+          }
+          </div>
+        </div>
+        <div className='flex hover:bg-gray-500 ml-5 p-2 pt-2 rounded'>
+          <Link to={"/register"}>
+            <img className='ml-1 mt-3 w-5 h-5' src={store} alt="" />
+          </Link>
+          <Link className='font-medium ml-2 mt-2' to={"/register"}>Register as Seller</Link>
         </div>
       </div>
-     </div>
   )
 }
 
