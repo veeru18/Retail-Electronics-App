aef6c2718bbbbd593a3529972f8e1c7db1c621d3
Veeresh T A <veereshta@gmail.com>
2024-04-14 01:58:33 +0530

    login and register page update@0.5

--- retail-electronics-app-UI/src/Util/Header.jsx
+++ retail-electronics-app-UI/src/Util/Header.jsx
@@ -11,6 +11,8 @@ import { HiMiniBars3BottomLeft } from 'react-icons/hi2';
 const Header = (props) => {
 
   let [isOpen, setIsOpen] = useState(false);
+  let [isMoreOpen, setIsMoreOpen] = useState(false);
+  const links = ["Contact Us", "Terms & Conditions"]
 
   const user = props?.userAuth;
 
@@ -20,9 +22,9 @@ const Header = (props) => {
     <nav className='bg-white shadow-md text-slate-100 py-2'>
       <div className="w-11/12 flex items-center justify-evenly">
         {/* logo and Link block */}
-        <div className='flex h-6 justify-center items-center w-3/6'>
+        <div className='flex h-6 justify-around items-center w-4/6'>
           {/* logo */}
-          <Link to={'/'} className='w-40'>
+          <Link to={'/'} className='ml-6 w-40'>
             <img src={logo} alt='logo' className='rounded w-full' />
           </Link>
 
@@ -46,10 +48,10 @@ const Header = (props) => {
               username
               : <div className=''>Login
                 {isOpen && (
-                  <div className='absolute p-2 mt-3 -ml-8'>
+                  <div className='absolute p-2 mt-3 -ml-12'>
                     <HeaderLink path={'/register'} linkName={'New Customer? SignUp'} />
                   </div>
-                  )
+                )
                 }
               </div>
             }
@@ -68,11 +70,18 @@ const Header = (props) => {
                   <HeaderLink linkName={"Become a Seller"} icon={<MdOutlineStore />} path={'/register'} />
               : <HeaderLink linkName={"Become a Seller"} icon={<MdOutlineStore />} path={'/register'} />
           }
-        </div>
-
-        <div className='flex'>
-          <HiMiniBars3BottomLeft className='' />
-          Hello
+          <div className='flex hover:bg-blue-500 p-3 rounded items-center'
+            onMouseEnter={() => { setIsMoreOpen((prev) => !prev) }} onMouseLeave={() => { setIsMoreOpen((prev) => !prev) }}>
+            <HiMiniBars3BottomLeft />
+            <div className='absolute p-2 mt-28 -ml-7'>
+              {isMoreOpen && links.map((item) => (
+                <div>
+                  <HeaderLink linkName={item} path={'/'} />
+                </div>
+              ))
+              }
+            </div>
+          </div>
         </div>
       </div>
     </nav>
@@ -84,9 +93,9 @@ export default Header
 export const HeaderLink = ({ icon, linkName, path }) => {
   return (
     <div>
-      <Link className='text-slate-600 flex justify-center items-center' to={path}>
-        <div className='mr-2'>{icon}</div>
-        <div className='mr-3'>{linkName}</div>
+      <Link className='text-slate-600 flex justify-start items-center' to={path}>
+        <div className='p-1 mr-1'>{icon}</div>
+        <div className='p-1 mr-2'>{linkName}</div>
       </Link>
     </div>
   );
